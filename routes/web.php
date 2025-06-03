7559b14161801adbe43886733049b771c26ea087
Kisharini <224618@student.upm.edu.my>
2025-06-03 13:42:18 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,38 @@
+<?php
 
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/homepage', function () {
+    return view('homepage');
+});
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+Route::get('/reward', function () {
+    return view('reward');
+});
+
+Route::get('/search', function () {
+    return view('search');
+});
